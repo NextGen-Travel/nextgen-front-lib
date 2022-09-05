92cc745bbc6b1d3c99549129ef0a0eaa7fb3fb87
zhihao <zhihaozheng@nextgen.com.hk>
2022-09-05 11:28:04 +0800

    update

---
+++ vue/views/app.vue
@@ -0,0 +1,12 @@
+<template>
+    <v-app>
+        <Confirm></Confirm>
+        <Notification></Notification>
+        <slot></slot>
+    </v-app>
+</template>
+
+<script setup lang="ts">
+import Confirm from './confirm.vue'
+import Notification from './notification.vue'
+</script>
