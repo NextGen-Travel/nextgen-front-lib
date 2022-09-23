673c4a895148d2e6dd1d2662056d0e7b0d0b9b25
nextgen <nextgen@khczhihaodeMacBook-Air.local>
2022-09-23 18:06:27 +0800

    update

--- utils/message-parser.ts
+++ utils/message-parser.ts
@@ -9,8 +9,8 @@ export const parseMessage = (data: any, def: string): string => {
         return message.response.data
     }
     if (message && message.response && pick.getType(message.response.data) === 'object') {
-        if (message.response.data.errors && message.response.data.errors.message) {
-            return message.response.data.errors.message
+        if (message.response.data.error && message.response.data.error.message) {
+            return message.response.data.error.message
         }
     }
     if (message && typeof message.message === 'string') {
