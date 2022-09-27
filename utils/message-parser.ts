1754ad992d513a58521d7f947afecfa439ff374f
nextgen <nextgen@khczhihaodeMacBook-Air.local>
2022-09-27 10:30:02 +0800

    update

--- utils/message-parser.ts
+++ utils/message-parser.ts
@@ -1,23 +1,22 @@
-import { pick } from 'power-helper'
-
 export const parseMessage = (data: any, def: string): string => {
     if (typeof data === 'string') {
         return data
     }
-    let message = data.message
-    if (message && message.response && typeof message.response.data === 'string') {
-        return message.response.data
-    }
-    if (message && message.response && pick.getType(message.response.data) === 'object') {
-        if (message.response.data.error && message.response.data.error.message) {
-            return message.response.data.error.message
+    // Axios
+    if (data.name === 'AxiosError') {
+        if (data.response && typeof data.response.data === 'string') {
+            return data.response.data
+        }
+        if (data.response.error && data.response.message) {
+            return data.response.message
         }
     }
-    if (message && typeof message.message === 'string') {
-        return message.message
-    }
-    if (typeof message === 'string') {
-        return message
+    // Gql
+    let message = data.message
+    if (message) {
+        if (typeof message === 'string') {
+            return message
+        }
     }
     return def || 'unknown error'
 }
