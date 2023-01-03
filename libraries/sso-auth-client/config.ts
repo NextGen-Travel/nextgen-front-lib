a14abd01fd022573add85b4ab7fd6fba2b5eaa9a
nextgen <nextgen@khczhihaodeMacBook-Air.local>
2023-01-03 12:47:47 +0800

    feat: sso nextgen endpoint@1672721267

--- libraries/sso-auth-client/config.ts
+++ libraries/sso-auth-client/config.ts
@@ -19,7 +19,7 @@ export const config: Record<Stages, {
                 url: 'https://cas-api-dev.cloudsatlas.com.hk/api',
                 scrmUrl: 'https://scrm-api-dev.cloudsatlas.com.hk/api',
                 dispensingUrl: 'https://medicine-api-dev.cloudsatlas.com.hk/api',
-                endpoint: 'https://login-dev.cloudsatlas.com.hk'
+                endpoint: 'https://login-dev.nextgen.com.hk'
             }
         }
     },
@@ -35,7 +35,7 @@ export const config: Record<Stages, {
                 url: 'https://cas-api-stage.cloudsatlas.com.hk/api',
                 scrmUrl: 'https://scrm-api-stage.cloudsatlas.com.hk/api',
                 dispensingUrl: 'https://medicine-api-stage.cloudsatlas.com.hk/api',
-                endpoint: 'https://login-stage.cloudsatlas.com.hk'
+                endpoint: 'https://login-stage.nextgen.com.hk'
             }
         }
     },
@@ -51,7 +51,7 @@ export const config: Record<Stages, {
                 url: 'https://cas-api.cloudsatlas.com.hk/api',
                 scrmUrl: 'https://scrm-api.cloudsatlas.com.hk/api',
                 dispensingUrl: 'https://medicine-api.cloudsatlas.com.hk/api',
-                endpoint: 'https://login.cloudsatlas.com.hk'
+                endpoint: 'https://login.nextgen.com.hk'
             }
         }
     }
