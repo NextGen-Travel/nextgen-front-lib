e608354f55ff0f08aded3a607a20009f7eab9b0c
nextgen <nextgen@khczhihaodeMacBook-Air.local>
2023-02-15 11:43:48 +0800

    feat: 例行更新@1676432628

--- core/vue/persist-state.ts
+++ core/vue/persist-state.ts
@@ -8,8 +8,8 @@ type Params = {
 }
 
 export class PersistStateManager {
-    params: Params
-    aliveKeys: string[] = []
+    private params: Params
+    private aliveKeys: string[] = []
 
     constructor(params: Params) {
         this.params = params
@@ -19,19 +19,24 @@ export class PersistStateManager {
         return this.params.ns() + '/' + key
     }
 
-    create<T extends Record<string, unknown>>(key: string, data: T): T {
-        let _key=  this.toKey(key)
+    create<T extends Record<string, unknown>>(key: string, data: () => T): T {
+        const storage = usePersistDataStorage()
+        const _key=  this.toKey(key)
         if (this.aliveKeys.includes(_key)) {
             throw serviceException.create(`Persist state key "${key}" already used.`)
         } else {
             this.aliveKeys.push(_key)
         }
     
-        const storage = usePersistDataStorage()
-        const origin = storage.get(_key)
-        const state = reactive(record.setMapValue(data, origin))
+        let origin: any
+        try {
+            origin = storage.get(_key)
+        } catch (error) {
+            origin = data()
+        }
 
-        watch(() => state, () => storage.set(_key, data), {
+        const state = reactive(record.setMapValue(data(), origin))
+        watch(() => state, () => storage.set(_key, state), {
             deep: true
         })
 
