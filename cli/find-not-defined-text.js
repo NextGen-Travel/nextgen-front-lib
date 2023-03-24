d33a618ac298b303b841dfd3eaeeb919e56da114
nextgen <nextgen@khczhihaodeMacBook-Air.local>
2023-03-24 12:16:30 +0800

    feat: 例行更新@1679631390

--- cli/find-not-defined-text.js
+++ cli/find-not-defined-text.js
@@ -4,6 +4,12 @@ const fsx = require('fs-extra')
 const { glob } = require('glob')
 const { pick, text } = require('power-helper')
 
+function extractFirstParam(text) {
+    const regex = /t\(['"]([^'"]+)['"]/
+    const matchs = text.match(regex)
+    return matchs
+}
+
 /**
  * @param {{ path: string }} params 
  */
@@ -12,23 +18,18 @@ module.exports = async(params = {
     path: '.'
 }) => {
     const files = await glob(`${params.path}/**/*.{js,ts,vue}`, { ignore: 'node_modules/**' })
-    const outputs = {
-        'zh-TW': {},
-        'zh-CN': {},
-        'en-US': {}
-    }
+    const outputs = {}
     for (let file of files) {
         const content = fsx.readFileSync(file, 'utf8')
-        const vars = pick.vars({
-            start: 't(\'##',
-            end: ')',
-            text: content
-        })
+        // const vars = pick.vars({
+        //     start: 't(\'##',
+        //     end: ')',
+        //     text: content
+        // })
+        const vars = extractFirstParam(content)
         for (let v of vars) {
             let key = (text.lastMatch(v, '\'') ? v.slice(0, -1) : v).trim()
-            outputs['zh-TW'][key] = key
-            outputs['zh-CN'][key] = key
-            outputs['en-US'][key] = key
+            outputs[key] = key
         }
     }
     return outputs
