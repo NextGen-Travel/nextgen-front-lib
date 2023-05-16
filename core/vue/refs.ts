8436e60412acb723ac9b8603ab2a4f672bfd24c9
nextgen <nextgen@khczhihaodeMacBook-Air.local>
2023-05-16 17:51:24 +0800

    feat: 例行更新@1684230684

--- core/vue/refs.ts
+++ core/vue/refs.ts
@@ -1,4 +1,6 @@
-import { GlobalComponents, ref } from 'vue'
+import { ref } from 'vue'
+// eslint-disable-next-line vue/prefer-import-from-vue
+import type { GlobalComponents } from '@vue/runtime-core'
 
 export const refComponents = <K extends keyof GlobalComponents>(_key: K) => ref<InstanceType<GlobalComponents[K]>>()
 
