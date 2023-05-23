11d3c6992adabbd700b2ab53b323953230e29af2
nextgen <nextgen@khczhihaodeMacBook-Air.local>
2023-05-23 11:28:13 +0800

    feat: 例行更新@1684812493

--- core/vue/refs.ts
+++ core/vue/refs.ts
@@ -1,8 +1,3 @@
 import { GlobalComponents, ref } from 'vue'
 
 export const refComponents = <K extends keyof GlobalComponents>(_key: K) => ref<InstanceType<GlobalComponents[K]>>()
-
-// TODO: no doc
-export const getComponentsProps = <K extends keyof GlobalComponents>(_key: K, props: Partial<InstanceType<GlobalComponents[K]>['$props']>) => {
-    return props
-}
