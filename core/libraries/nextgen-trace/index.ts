069c94c948c66bc402763085ec4a372c84ce0a1f
nextgen <nextgen@khczhihaodeMacBook-Air.local>
2023-06-02 15:23:44 +0800

    feat: 例行更新@1685690624

---
+++ core/libraries/nextgen-trace/index.ts
@@ -0,0 +1,72 @@
+import axios from 'axios'
+import FingerprintJS from '@fingerprintjs/fingerprintjs'
+import { flow, Interaction, JobsQueue } from 'power-helper'
+import { useLibEnv } from '../../index'
+
+type Message = {
+    time: number
+    type: Parameters<Interaction['notify']>[0]
+    name: string
+    text: string
+}
+
+type SendData = {
+    fingerId: string
+    visitorId: string
+    url: string
+    service: string
+    stage: string
+    messages: Message[]
+}
+
+export class NextgenMessageTrace {
+    messages: Message[] = []
+    fingerId = ''
+    serverUrl = ''
+    visitorId = flow.createUuid()
+    jobsQueue = new JobsQueue({
+        concurrentExecutions: 1
+    })
+
+    setServerUrl(url: string) {
+        this.serverUrl = url
+    }
+
+    clearMessages() {
+        const messages = this.messages.slice()
+        this.messages = []
+        return messages
+    }
+
+    collect(message: Omit<Message, 'time'>) {
+        this.messages.push({
+            ...message,
+            time: Date.now()
+        })
+    }
+
+    send() {
+        this.jobsQueue.push('send', async() => {
+            if (this.fingerId === '') {
+                const fp = await FingerprintJS.load()
+                const result = await fp.get()
+                this.fingerId = result.visitorId
+            }
+            const messages = this.clearMessages()
+            const env = useLibEnv()
+            const sendData: SendData = {
+                fingerId: this.fingerId,
+                visitorId: this.visitorId,
+                url: window.location.href,
+                service: env.service,
+                stage: env.stage,
+                messages
+            }
+            if (messages.length !== 0) {
+                await axios.post(this.serverUrl, {
+                    body: sendData
+                })
+            }
+        })
+    }
+}
