2e770d2ea0a127b5382479171e8b1249d788556f
nextgen <nextgen@khczhihaodeMacBook-Air.local>
2023-06-13 15:06:23 +0800

    feat: 例行更新@1686639983

---
+++ core/compnents.ts
@@ -0,0 +1,45 @@
+import Ani from './vue/components/ani.vue'
+import App from '../core/vue/views/app.vue'
+import Map from '../core/vue/components/map.vue'
+import Dialog from '../core/vue/components/dialog.vue'
+import FixedBar from '../core/vue/components/fixed-bar.vue'
+import Form from '../core/vue/components/form.vue'
+import Img from '../core/vue/components/img.vue'
+import Loaders from '../core/vue/components/loaders.vue'
+import OutlineText from '../core/vue/components/outline-text.vue'
+import Pagination from '../core/vue/components/pagination.vue'
+import Table from '../core/vue/components/table.vue'
+import Toolbar from '../core/vue/components/toolbar.vue'
+import Upload from '../core/vue/components/upload.vue'
+import Search from '../core/vue/components/search.vue'
+import Skeleton from '../core/vue/components/skeleton.vue'
+import DatePicker from '../core/vue/components/date-picker.vue'
+import OverlayLoading from '../core/vue/components/overlay-loading.vue'
+import VisibleLoad from '../core/vue/components/visible-load.vue'
+import DateRangePicker from '../core/vue/components/date-range-picker.vue'
+import PieChart from '../core/vue/components/charts/pie.vue'
+import DoughnutChart from '../core/vue/components/charts/doughnut.vue'
+import SkeletonGroup from '../core/vue/components/skeleton-group.vue'
+
+export const NgAni = Ani
+export const NgApp = App
+export const NgMap = Map
+export const NgDialog = Dialog
+export const NgFixedBar = FixedBar
+export const NgForm = Form
+export const NgImg = Img
+export const NgLoaders = Loaders
+export const NgOutlineText = OutlineText
+export const NgPagination = Pagination
+export const NgTable = Table
+export const NgToolbar = Toolbar
+export const NgUpload = Upload
+export const NgSearch = Search
+export const NgSkeleton = Skeleton
+export const NgDatePicker = DatePicker
+export const NgOverlayLoading = OverlayLoading
+export const NgVisibleLoad = VisibleLoad
+export const NgDateRangePicker = DateRangePicker
+export const NgPieChart = PieChart
+export const NgDoughnutChart = DoughnutChart
+export const NgSkeletonGroup = SkeletonGroup
