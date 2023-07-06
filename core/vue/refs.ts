ff9c19cd57560bd0faaeb4c4b3b07b46c249d46d
nextgen <nextgen@khczhihaodeMacBook-Air.local>
2023-07-06 12:26:14 +0800

    feat: 例行更新@1688617574

--- core/vue/refs.ts
+++ core/vue/refs.ts
@@ -1,6 +1,9 @@
 import { GlobalComponents, ref } from 'vue'
+import { NgComponents } from '../../types/index'
+
+type AllCompoents = GlobalComponents & NgComponents
 
 export const refComponents = <
-    K extends keyof GlobalComponents,
-    N = GlobalComponents[K] extends (abstract new (..._args: any) => any) ? InstanceType<GlobalComponents[K]> : unknown
+    K extends keyof AllCompoents,
+    N = AllCompoents[K] extends (abstract new (..._args: any) => any) ? InstanceType<AllCompoents[K]> : unknown
 >(_key: K) => ref<N>()
