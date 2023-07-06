9c4fefcf2957249508066a7989e6406f41668f89
nextgen <nextgen@khczhihaodeMacBook-Air.local>
2023-07-06 12:22:32 +0800

    feat: 例行更新@1688617352

--- core/vue/refs.ts
+++ core/vue/refs.ts
@@ -1,3 +1,6 @@
 import { GlobalComponents, ref } from 'vue'
 
-export const refComponents = <K extends keyof GlobalComponents>(_key: K) => ref<InstanceType<GlobalComponents[K] & (new (..._args: any[]) => any)>>()
+export const refComponents = <
+    K extends keyof GlobalComponents,
+    N = GlobalComponents[K] extends (abstract new (..._args: any) => any) ? InstanceType<GlobalComponents[K]> : unknown
+>(_key: K) => ref<N>()
--- types/index.d.ts
+++ types/index.d.ts
@@ -6,34 +6,36 @@ import 'google.maps'
 import 'google.accounts'
 import '../shims'
 
+export type NgComponents = {
+    NgApp: typeof import('../types-vue/core/vue/views/app.vue.d')['default']
+    NgMap: typeof import('../types-vue/core/vue/components/map.vue.d')['default']
+    NgDialog: typeof import('../types-vue/core/vue/components/dialog.vue.d')['default']
+    NgFixedBar: typeof import('../types-vue/core/vue/components/fixed-bar.vue.d')['default']
+    NgForm: typeof import('../types-vue/core/vue/components/form.vue.d')['default']
+    NgImg: typeof import('../types-vue/core/vue/components/img.vue.d')['default']
+    NgLoaders: typeof import('../types-vue/core/vue/components/loaders.vue.d')['default']
+    NgOutlineText: typeof import('../types-vue/core/vue/components/outline-text.vue.d')['default']
+    NgPagination: typeof import('../types-vue/core/vue/components/pagination.vue.d')['default']
+    NgTable: typeof import('../types-vue/core/vue/components/table.vue.d')['default']
+    NgToolbar: typeof import('../types-vue/core/vue/components/toolbar.vue.d')['default']
+    NgUpload: typeof import('../types-vue/core/vue/components/upload.vue.d')['default']
+    NgSearch: typeof import('../types-vue/core/vue/components/search.vue.d')['default']
+    NgSkeleton: typeof import('../types-vue/core/vue/components/skeleton.vue.d')['default']
+    NgDatePicker: typeof import('../types-vue/core/vue/components/date-picker.vue.d')['default']
+    NgOverlayLoading: typeof import('../types-vue/core/vue/components/overlay-loading.vue.d')['default']
+    NgVisibleLoad: typeof import('../types-vue/core/vue/components/visible-load.vue.d')['default']
+    NgDateRangePicker: typeof import('../types-vue/core/vue/components/date-range-picker.vue.d')['default']
+    NgPieChart: typeof import('../types-vue/core/vue/components/charts/pie.vue.d')['default']
+    NgDoughnutChart: typeof import('../types-vue/core/vue/components/charts/doughnut.vue.d')['default']
+    NgSkeletonGroup: typeof import('../types-vue/core/vue/components/skeleton-group.vue.d')['default']
+    NgAni: typeof import('../types-vue/core/vue/components/ani.vue.d')['default']
+}
+
 declare module 'vue' {
     export interface ComponentCustomProperties {
         $t: (_key: string, ..._args: any[]) => string
     }
-    export interface GlobalComponents {
-        NgApp: typeof import('../types-vue/core/vue/views/app.vue')['default']
-        NgMap: typeof import('../types-vue/core/vue/components/map.vue')['default']
-        NgDialog: typeof import('../types-vue/core/vue/components/dialog.vue')['default']
-        NgFixedBar: typeof import('../types-vue/core/vue/components/fixed-bar.vue')['default']
-        NgForm: typeof import('../types-vue/core/vue/components/form.vue')['default']
-        NgImg: typeof import('../types-vue/core/vue/components/img.vue')['default']
-        NgLoaders: typeof import('../types-vue/core/vue/components/loaders.vue')['default']
-        NgOutlineText: typeof import('../types-vue/core/vue/components/outline-text.vue')['default']
-        NgPagination: typeof import('../types-vue/core/vue/components/pagination.vue')['default']
-        NgTable: typeof import('../types-vue/core/vue/components/table.vue')['default']
-        NgToolbar: typeof import('../types-vue/core/vue/components/toolbar.vue')['default']
-        NgUpload: typeof import('../types-vue/core/vue/components/upload.vue')['default']
-        NgSearch: typeof import('../types-vue/core/vue/components/search.vue')['default']
-        NgSkeleton: typeof import('../types-vue/core/vue/components/skeleton.vue')['default']
-        NgDatePicker: typeof import('../types-vue/core/vue/components/date-picker.vue')['default']
-        NgOverlayLoading: typeof import('../types-vue/core/vue/components/overlay-loading.vue')['default']
-        NgVisibleLoad: typeof import('../types-vue/core/vue/components/visible-load.vue')['default']
-        NgDateRangePicker: typeof import('../types-vue/core/vue/components/date-range-picker.vue')['default']
-        NgPieChart: typeof import('../types-vue/core/vue/components/charts/pie.vue')['default']
-        NgDoughnutChart: typeof import('../types-vue/core/vue/components/charts/doughnut.vue')['default']
-        NgSkeletonGroup: typeof import('../types-vue/core/vue/components/skeleton-group.vue')['default']
-        NgAni: typeof import('../types-vue/core/vue/components/ani.vue')['default']
-    }
+    export type GlobalComponents = NgComponents
 }
 
 declare global {
