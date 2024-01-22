b8cda6df12cb49b03663611fa079eb8dce039f0d
zhihao <zhihaozheng@nextgen.com.hk>
2024-01-22 14:37:46 +0800

    feat: 例行更新@1705905466

--- core/components.ts
+++ core/components.ts
@@ -1,8 +1,14 @@
 import 'vue'
 import * as components from './components/index'
+import * as layouts from './layouts/index'
 
-export type NgComponents<T extends keyof typeof components = keyof typeof components> = {
+export type NgComponents<
+    T extends keyof typeof components = keyof typeof components,
+    L extends keyof typeof layouts = keyof typeof layouts
+> = {
     [K in T]: typeof components[K]
+} & {
+    [K in L]: typeof layouts[K]
 }
 
 declare module 'vue' {
--- types/components.d.ts
+++ types/components.d.ts
@@ -1,7 +1,10 @@
 import 'vue';
 import * as components from './components/index';
-export type NgComponents<T extends keyof typeof components = keyof typeof components> = {
+import * as layouts from './layouts/index';
+export type NgComponents<T extends keyof typeof components = keyof typeof components, L extends keyof typeof layouts = keyof typeof layouts> = {
     [K in T]: typeof components[K];
+} & {
+    [K in L]: typeof layouts[K];
 };
 declare module 'vue' {
     interface GlobalComponents extends NgComponents {
