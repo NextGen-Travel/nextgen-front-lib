bc9f529be7a9289f70173a8c2ec793cbe4a95009
zhihao <zhihaozheng@nextgen.com.hk>
2024-01-24 15:15:48 +0800

    feat: 例行更新@1706080548

--- core/index.ts
+++ core/index.ts
@@ -6,9 +6,12 @@ import * as components from './components/index'
 import * as layouts from './layouts/index'
 
 // chart
-import { Chart, DoughnutController, LineController, PieController } from 'chart.js'
+import { Chart, DoughnutController, ArcElement, LineController, LineElement, PointElement, PieController } from 'chart.js'
 
 Chart.register(
+    ArcElement,
+    LineElement,
+    PointElement,
     DoughnutController,
     LineController,
     PieController
